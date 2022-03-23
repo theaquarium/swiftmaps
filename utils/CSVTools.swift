cd78855ee0961323bf8a5eb624ee58d97c7952b2
Leah Vashevko <leah@vashevko.com>
2022-03-23 15:02:33 -0400

    Modify CSV

--- utils/CSVTools.swift
+++ utils/CSVTools.swift
@@ -1,7 +1,7 @@
 func createCSV(_ input: (getResults: [Int], setResults: [Int])) -> String {
     let (getResults, setResults) = input
 
-    var result = "Gets, Sets\n"
+    var result = "gets,sets\n"
     for i in 0..<getResults.count {
         result += "\(getResults[i]),\(setResults[i])\n"
     }
